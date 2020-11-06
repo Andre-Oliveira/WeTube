155d51607cb5eae0c61880d0465ba64121786b15
André Oliveira <andre.a.c.oliveira@outlook.com>
2020-11-06 17:45:53 +0000

    repo reorganizado

--- WeTube/WeTube/urls.py
+++ WeTube/WeTube/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 #Plugin imports
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 #App imports
 from . import views
 from .views import userViewSet
@@ -12,6 +13,7 @@ router.register('users', userViewSet)
 urlpatterns = [
     #path('', views.index, name='index'),
     path('', include(router.urls)),
+    path('auth/', obtain_auth_token),
     path('admin/', admin.site.urls),
     path('video/', include('Video.urls')),
     path('audio/', include('Audio.urls')),
